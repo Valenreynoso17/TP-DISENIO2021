a4997affeb0414b804cad676400f3065231a2be9
FedericoQuarin <federicoquarin2@gmail.com>
2021-10-15 11:21:59 -0300

    Agregados constructores a PostgreImpl

--- src/main/java/postgreImpl/LocalidadPostgreSQLImpl.java
+++ src/main/java/postgreImpl/LocalidadPostgreSQLImpl.java
@@ -1,7 +1,13 @@
 package main.java.postgreImpl;
 
+import org.hibernate.SessionFactory;
+
 import main.java.daos.LocalidadDAO;
 
 public class LocalidadPostgreSQLImpl implements LocalidadDAO {
-
+	private SessionFactory sessionFactory;
+	
+	public LocalidadPostgreSQLImpl() {
+		sessionFactory = HibernateManager.Configure();
+	}
 }
--- src/main/java/postgreImpl/PaisPostgreSQLImpl.java
+++ src/main/java/postgreImpl/PaisPostgreSQLImpl.java
@@ -1,7 +1,13 @@
 package main.java.postgreImpl;
 
+import org.hibernate.SessionFactory;
+
 import main.java.daos.PaisDAO;
 
 public class PaisPostgreSQLImpl implements PaisDAO {
-
+	private SessionFactory sessionFactory;
+	
+	public PaisPostgreSQLImpl() {
+		sessionFactory = HibernateManager.Configure();
+	}
 }
