69b6f7757a34762a4d9c12d65a3c324575e8cf7b
FedericoQuarin <federicoquarin2@gmail.com>
2021-10-17 21:01:23 -0300

    Constructores, getters y setters

--- src/main/java/clases/ResponsableDePago.java
+++ src/main/java/clases/ResponsableDePago.java
@@ -1,7 +1,41 @@
 package main.java.clases;
 
 public class ResponsableDePago {
-	private String id;
+	private Integer id;
 	private String razonSocial;
+	
+	
+	public ResponsableDePago() {
+		super();
+	}
+
+	public ResponsableDePago(Integer id, String razonSocial) {
+		super();
+		this.id = id;
+		this.razonSocial = razonSocial;
+	}
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	
+	
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	
+	
+	
 	
 }
