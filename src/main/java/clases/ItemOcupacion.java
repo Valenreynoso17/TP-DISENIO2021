e89bd24e0a67bd6a151522ed6724a51001556379
FedericoQuarin <federicoquarin2@gmail.com>
2021-11-28 16:30:10 -0300

    Algo de mapeo y agregados equals y hashCode

--- src/main/java/clases/ItemOcupacion.java
+++ src/main/java/clases/ItemOcupacion.java
@@ -2,8 +2,17 @@ package main.java.clases;
 
 import javax.persistence.*;
 
+@Entity
+@Table(name="disenio.itemocupacion")
 public class ItemOcupacion extends ItemFactura {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "cantidaddias", nullable = false, unique = false)
 	private Integer cantidadDias;
+	
+	@ManyToOne()
+	@JoinColumn(name = "idocupacion", referencedColumnName = "id")
 	private Ocupacion ocupacion;
 }
--- src/main/java/clases/Localidad.java
+++ src/main/java/clases/Localidad.java
@@ -55,6 +55,43 @@ public class Localidad {
 	public void setProvincia(Provincia provincia) {
 		this.provincia = provincia;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((provincia == null) ? 0 : provincia.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Localidad other = (Localidad) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (provincia == null) {
+			if (other.provincia != null)
+				return false;
+		} else if (!provincia.equals(other.provincia))
+			return false;
+		return true;
+	}
 	
 	
 	
