6ea34c4f25cd476e2b4f3cf8a5bc14a79edec46d
FedericoQuarin <federicoquarin2@gmail.com>
2021-11-29 10:34:32 -0300

    Ahora si terminado

--- src/main/java/clases/PersonaJuridica.java
+++ src/main/java/clases/PersonaJuridica.java
@@ -1,9 +1,29 @@
 package main.java.clases;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="disenio.personajuridica")
 public class PersonaJuridica {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "cuit", nullable = false, unique = false)
 	private String cuit;
+	
+	@Column(name = "telefono", nullable = false, unique = false)
 	private String telefono;
+	
+	@OneToOne
+	@JoinColumn(name = "iddireccion", referencedColumnName = "id")
 	private Direccion direccion;
 	
 	
