c1d2168557b07dab506b07c889eca9cb0de9faab
Julio Chort <julioch_17@outlook.com>
2021-11-30 21:33:05 -0300

    Mensajes finales y primera prueba de grilla

---
+++ src/main/java/main/PRUEBA_GRID.java
@@ -0,0 +1,38 @@
+package main.java.main;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+	
+class PRUEBA_GRID extends JPanel {
+	
+	 int width, height;
+	
+	 int rows;
+	
+	 int cols;
+
+	 PRUEBA_GRID(int w, int h, int r, int c) {
+	    setSize(width = w, height = h);
+	    rows = r;
+	    cols = c;
+	 }
+	
+	 public void paint(Graphics g) {
+	    int i;
+	    width = getSize().width;
+	    height = getSize().height;
+	
+	    // draw the rows
+	    int rowHt = height / (rows);
+	    for (i = 0; i < rows; i++)
+	      g.drawLine(0, i * rowHt, width, i * rowHt);
+	
+	    // draw the columns
+	    int rowWid = width / (cols);
+	    for (i = 0; i < cols; i++)
+	      g.drawLine(i * rowWid, 0, i * rowWid, height);
+	 }
+}
+
