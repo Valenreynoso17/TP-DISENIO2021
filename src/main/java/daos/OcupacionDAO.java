2bad0e042aec03d9437954e54e15c97eb8cb0547
FedericoQuarin <federicoquarin2@gmail.com>
2021-12-01 19:49:20 -0300

    Metodos para recuperar entre fechas ocupaciones, reservas y fueras de servicio

--- src/main/java/daos/OcupacionDAO.java
+++ src/main/java/daos/OcupacionDAO.java
@@ -1,5 +1,8 @@
 package main.java.daos;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import main.java.clases.Ocupacion;
 
 public interface OcupacionDAO {
@@ -7,4 +10,5 @@ public interface OcupacionDAO {
 	public Ocupacion buscar(Integer id);
 	public Ocupacion buscarExtendido(Integer id);
 	public Ocupacion buscarUltimaOcupacion(Integer nroHabitacion);
+	public List<Ocupacion> buscar(LocalDate fechaDesde, LocalDate fechaHasta);
 }
